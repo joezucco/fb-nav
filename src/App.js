ab393636cc2adeee7098cba3c15522f59600022b
Joe Zucco <josephzucco@gmail.com>
2021-10-05 14:10:52 -0400

    set dropdown state

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
 import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
 import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
 
+import React, { useState } from "react";
+
 function App() {
   return (
     <Navbar>
       <NavItem icon={<PlusIcon />} />
       <NavItem icon={<BellIcon />} />
       <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CaretIcon />}>{/* Dropdown goes here */}</NavItem>
     </Navbar>
   );
 }
@@ -27,9 +31,11 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
   return (
     <li className='nav-item'>
-      <a href='#' className='icon-button'>
+      <a href='#' className='icon-button' onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
     </li>
